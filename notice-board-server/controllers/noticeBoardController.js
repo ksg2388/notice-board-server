7565e1c941a09fe6034e03abb734b88eb7ad8a5e
ksg2388 <ksg2388@naver.com>
2022-11-09 23:39:31 +0900

    feat(noticeBoard): 게시글 페이징 기능 구현

--- notice-board-server/controllers/noticeBoardController.js
+++ notice-board-server/controllers/noticeBoardController.js
@@ -12,6 +12,33 @@ const getAllPost = asyncHandler(async (req, res) => {
   res.json(posts);
 });
 
+// @desc Get 페이지별 post
+// @route GET /post/:perPage/:maxPage
+// @access Private
+const getPost = asyncHandler(async (req, res) => {
+  const page = Number(req.query.page || 1);
+  const perPage = Number(req.query.perPage || 10);
+
+  console.log(req.query);
+
+  const total = await NoticeBoard.countDocuments({}); // 총 게시글 수 세기
+  const posts = await NoticeBoard.find({})
+    .sort({ createdAt: -1 }) // 생성한 시간을 역순으로 정렬
+    .skip(perPage * (page - 1)) // 검색 시 포함하지 않을 데이터 수
+    .limit(perPage); // 검색 결과 수 제한
+  const totalPage = Math.ceil(total / perPage);
+
+  if (!posts?.length) {
+    return res.status(400).json({ message: "No Posts found" });
+  }
+
+  res.json({
+    posts: posts,
+    currentPage: page,
+    maxPage: totalPage,
+  });
+});
+
 // @desc Create new post
 // @route POST /post
 // @access Private
@@ -71,4 +98,5 @@ module.exports = {
   getAllPost,
   createNewPost,
   deletePost,
+  getPost,
 };
