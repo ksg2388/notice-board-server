415eb783e2c6fb4bb65a6dccac42837332b9428e
ksg2388 <ksg2388@naver.com>
2022-11-09 17:14:31 +0900

    fix(noticeBoard): 스키마에 자동으로 생성날짜, 수정날짜 포함되도록 변경

--- notice-board-server/models/noticeBoard.js
+++ notice-board-server/models/noticeBoard.js
@@ -1,25 +1,26 @@
 const mongoose = require("mongoose");
 
-const noticeBoardSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const noticeBoardSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: [String],
+    },
+    user: {
+      id: String,
+      username: String,
+    },
   },
-  content: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: [String],
-  },
-  publishedDate: {
-    type: Date,
-    default: Date.now, // 현재 날짜를 기본값으로 지정
-  },
-  user: {
-    id: String,
-    username: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("NoticeBoard", noticeBoardSchema);
--- notice-board-server/routes/userRoutes.js
+++ notice-board-server/routes/userRoutes.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/usersController");
+const verifyJWT = require("../middleware/verifyJWT");
 
 router
   .route("/")
   .get(userController.getAllUsers)
   .post(userController.createNewUser);
-// .patch(userController.updateUser)
-// .delete(userController.deleteUser);
+// .patch(verifyJWT, userController.updateUser);
+//   .delete(userController.deleteUser);
+
+// router.use(verifyJWT);
+
+// router
+//   .route("/")
 
 module.exports = router;
